660f9698247934f985b103da02a19347e768ebc6
illa <ilia.verstuk@gmail.com>
2025-03-01 14:16:40 +0200

    1

---
+++ 01-operators-type-if/test.js
@@ -0,0 +1,20 @@
+// коментар 
+
+/* 
+коментар
+*/
+
+// зміна
+let name = "illa";
+let number = 423;
+let numberR = 345;
+let fullName = 'ілля Верстюк';
+let myList = [1, 2, 3, 7130];
+let meDic = {'name':'', age:0};
+let b = true;
+
+number = number + numberR;
+
+const NAME = "ILLA"
+
+
